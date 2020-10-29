665ba42d149d9fb4ab4e1518b859794c0ce6788a
vip <vip@email.com>
2020-10-29 18:20:42 +0800

    task: 7 添加pom依赖和一些基础架构

--- web/src/main/java/com/project/name/ProjectNameApplication.java
+++ web/src/main/java/com/project/name/ProjectNameApplication.java
@@ -1,4 +1,12 @@
 package com.project.name;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ProjectNameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectNameApplication.class);
+    }
 }
---
+++ web/src/main/java/com/project/name/web/controller/cars/GoodsController.java
@@ -0,0 +1,20 @@
+package com.project.name.web.controller.cars;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName: GoodsController <br/>
+ * Description: TODO
+ * Date 2020/10/29 17:55
+ *
+ * @author Lenovo
+ **/
+
+@RestController
+public class GoodsController {
+
+
+
+
+
+}
